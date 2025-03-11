1de8a34575e08d87f2fbbc266b0b90e563144979
587712 <587712@stud.hvl.no>
2025-03-11 13:52:11 +0100

    Oppretta testklasse for MengdeADT

---
+++ DAT102-Oblig3/src/test/MengdeADTTest.java
@@ -0,0 +1,19 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class MengdeADTTest {
+	
+	@BeforeEach
+	void setUp() {}
+	
+	@Test
+	void testErTom() {}
+	
+	@Test
+	void testErMed() {}
+	
+}
