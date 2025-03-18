1cb896d9e2a9d84fc8bd3f35c3a555891848c620
Kristian-LHaug <186473@stud.hvl.no>
2025-03-18 13:59:15 +0100

    fikset alt i samme klassen

--- DAT102-Oblig3/src/Uke11_Oppg4/HashSetOgTabellSok.java
+++ DAT102-Oblig3/src/Uke11_Oppg4/HashSetOgTabellSok.java
@@ -1,21 +1,27 @@
 package Uke11_Oppg4;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Random;
 
-public class HashSetSok {
+public class HashSetOgTabellSok {
 
 	public static void main(String[] args) {
 		 
 		int antElmt = 100000;
 		int maxVerdi = 1000000;
 		
+		int[] tabell = new int[antElmt];
 		HashSet<Integer> hashSet = new HashSet<>();
+		
 		int tall = 376; 
         for (int i = 0; i < antElmt; i++) {
+        	tabell[i] = tall;
 			hashSet.add(tall);
 			tall = (tall + 45713) % maxVerdi;
         }
+        Arrays.sort(tabell);
+        
         
     	int antallSok = 10000;
     	Random tilfeldig = new Random();
@@ -25,17 +31,33 @@ public class HashSetSok {
     		}
     	
     	long startTid = System.nanoTime();
+		int funnetSok = 0; 	
+		for(int tallSok : sokTab) {
+			if (Arrays.binarySearch(tabell, tallSok) >= 0) {
+					funnetSok++;
+			}
+		}
+		long sluttTid = System.nanoTime();
+		long Tid = sluttTid - startTid;
+    	
+    	
+		
+    	long startTid2 = System.nanoTime();
     	int funnet = 0; 
     	for (int tallSok : sokTab) {
     		if (hashSet.contains(tallSok)) {
     			funnet++; 
     		}
     	}
-    	long sluttTid = System.nanoTime();
-    	long tid = sluttTid - startTid;
+    	long sluttTid2 = System.nanoTime();
+    	long tid2 = sluttTid2 - startTid2;
     	
+    	
+
+		System.out.println("Antall treff med binærsøk: " + funnetSok);
+		System.out.println("Tid for binærsøk = " + Tid/1000000.0 + "ms");
     	System.out.println("Antall treff med søk i HashSet: " + funnet);
-    	System.out.println("Tid med sok i HashSet: " + tid/1000000.0 + "ms");
+    	System.out.println("Tid med sok i HashSet: " + tid2/1000000.0 + "ms");
 	}
 	
 }
